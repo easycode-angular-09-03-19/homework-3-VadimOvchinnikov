fffce57f3d91c8b009a760ceaa68dda03929506d
Vadim Ovchinnikov <sodium@nodead.net>
2019-03-20 14:48:37 +0200

    Added solution.

---
+++ src/app/car.ts
@@ -0,0 +1,43 @@
+import { validateNumberValue } from './helpers/helper';
+
+export class Car {
+  constructor(private _name: string, private _mileage: number, private _fuel: number, private _tankCapacity: number, private _fuelPerKilometer: number, private _features: Array<string>) {
+    validateNumberValue(_mileage, "mileage");
+    validateNumberValue(_fuel, "fuel");
+    validateNumberValue(_tankCapacity, "tank capacity");
+    validateNumberValue(_fuelPerKilometer, "fuel per kilometre");
+
+    if (_fuel > _tankCapacity) throw new Error("Fuel amount cannot be more than tank capacity.");
+  }
+
+  public drive(distance: number): void {
+    validateNumberValue(distance, "distance");
+    const fuelSpent = Math.min(this._fuelPerKilometer * distance, this._fuel);
+    this._fuel -= fuelSpent;
+    this._mileage += fuelSpent / this._fuelPerKilometer;
+
+    if (this.isEmptyTank) console.log("You need to refuel.");
+  }
+
+  public refuel(fuel: number): void {
+    validateNumberValue(fuel, "quantity");
+
+    if (this._fuel + fuel > this._tankCapacity) throw new Error("Fuel amount cannot be more than tank capacity.");
+
+    this._fuel += fuel;
+  }
+
+  public get name() { return this._name; }
+
+  public get mileage() { return this._mileage; }
+
+  public get fuel() { return this._fuel; }
+
+  public get isEmptyTank() { return this.fuel <= 0.001; }
+
+  public get tankCapacity() { return this._tankCapacity; }
+
+  public get fuelPerKilometer() { return this._fuelPerKilometer; }
+
+  public get features() { return this._features; }
+}
---
+++ src/app/car/car.component.ts
@@ -0,0 +1,101 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Car } from '../car';
+
+@Component({
+  selector: 'app-car',
+  templateUrl: './car.component.html',
+  styleUrls: ['./car.component.css']
+})
+export class CarComponent implements OnInit {
+  public cars: Array<Car> = [new Car("BMW", 100, 20, 40, 0.08, ["Compfortable seats", "Nice design"]), new Car("Volvo", 2000, 5, 40, 0.05, ["Safety features"])];
+  public isAddCarDialogVisible: boolean = false;
+  public newCarForm: FormGroup;
+
+  public currentCar: Car | null = null;
+  public isActionDialogVisible: boolean = false;
+  public actionForm: FormGroup;
+  public actionName: string | null = null;
+  public actionHeader: string | null = null;
+  public actionLabel: string | null = null;
+
+  constructor() { }
+
+  public get nameTextBox() { return this.newCarForm.controls.nameTextBox; }
+  public get mileageTextBox() { return this.newCarForm.controls.mileageTextBox; }
+  public get fuelTextBox() { return this.newCarForm.controls.fuelTextBox; }
+  public get tankCapacityTextBox() { return this.newCarForm.controls.tankCapacityTextBox; }
+  public get fuelPerKilometerTextBox() { return this.newCarForm.controls.fuelPerKilometerTextBox; }
+  public get featuresTextBox() { return this.newCarForm.controls.featuresTextBox; }
+
+  public get quantityTextBox() { return this.actionForm.controls.quantityTextBox; }
+
+  public ngOnInit() {
+    const numberPattern: string = "^[0-9]+(\.[0-9]+)?$";
+    this.newCarForm = new FormGroup({
+      nameTextBox: new FormControl(null, [Validators.required]),
+      mileageTextBox: new FormControl(null, [Validators.required, Validators.pattern(numberPattern)]),
+      fuelTextBox: new FormControl(null, [Validators.required, Validators.pattern(numberPattern)]),
+      tankCapacityTextBox: new FormControl(null, [Validators.required, Validators.pattern(numberPattern)]),
+      fuelPerKilometerTextBox: new FormControl(null, [Validators.required, Validators.pattern(numberPattern)]),
+      featuresTextBox: new FormControl()
+    });
+
+    this.actionForm = new FormGroup({
+      quantityTextBox: new FormControl(null, [Validators.required, Validators.pattern(numberPattern)])
+    });
+  }
+
+  public openAddCarDialog() { this.isAddCarDialogVisible = true; }
+
+  public closeAddCarDialog() { this.isAddCarDialogVisible = false; }
+
+  public addCar() {
+    this.cars.push(new Car(this.nameTextBox.value, parseFloat(this.mileageTextBox.value), parseFloat(this.fuelTextBox.value),
+      parseFloat(this.tankCapacityTextBox.value), parseFloat(this.fuelPerKilometerTextBox.value), (this.featuresTextBox.value || "").split(/\r?\n/)));
+    this.isAddCarDialogVisible = false;
+  }
+
+  public drive(car: Car) {
+    this.isActionDialogVisible = true;
+    this.actionName = "DRIVE";
+    this.actionHeader = "Driving car";
+    this.actionLabel = "Distance, km";
+    this.currentCar = car;
+  }
+
+  public refuel(car: Car) {
+    this.isActionDialogVisible = true;
+    this.actionName = "REFUEL";
+    this.actionHeader = "Refueling car";
+    this.actionLabel = "Fuel, liters";
+    this.currentCar = car;
+  }
+
+  public closeActionDialog() {
+    this.isActionDialogVisible = false;
+    this.actionName = null;;
+    this.actionHeader = null;
+    this.actionLabel = null;
+    this.currentCar = null;
+    this.quantityTextBox.setValue(null);
+  }
+
+  public permormAction() {
+    const quantity = parseFloat(this.quantityTextBox.value);
+
+    switch (this.actionName) {
+      case "DRIVE":
+        this.currentCar.drive(quantity);
+        break;
+
+      case "REFUEL":
+        this.currentCar.refuel(quantity);
+        break;
+
+      default:
+        throw new Error("Unknown action type.");
+    }
+    this.closeActionDialog();
+  }
+}
